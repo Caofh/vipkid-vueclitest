736b51802a3974135e2475d5f267727ae2182510
caofanghui <caofanghui@vipkid.com.cn>
2018-05-29 14:05:34 +0800

    update-FirstVersion

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    123
+    <img src="../assets/img/@1x.png">
+    <img src="../assets/img/@2x.png">
+    <img src="../assets/img/@3x.png">
+  </div>
+</template>
+
+<script>
+import '../assets/js/m.js'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'I am home page !'
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  @import '../assets/css/pr2rem';
+
+  .home {
+    img {
+      width: pr(750);
+    }
+  }
+
+</style>
